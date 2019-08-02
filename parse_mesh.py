57f6183c5c81f8ec6370550d956862d25d3b0685
Kirk Gasper <wigasper@gmail.com>
2019-08-02 13:44:52 -0500

    working on refactor

--- parse_mesh.py
+++ parse_mesh.py
@@ -1,52 +1,47 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
-from tqdm import tqdm
-records = []
+import re
 
-with open("../data/desc2019", "r") as handle:
-    record = ""
-    for line in handle:
-        if line.startswith("<DescriptorRecord"):
-            if record:
-                records.append(record)
-                record = ""
-            record = "".join([record, line])
-        else:
-            record = "".join([record, line])
+def parse_mesh(descriptor_file):
+    desc_uis = []
+    desc_names = []
+    tree_num_lists = []
 
-# Discard first 2 records which have document info
-records = records[2:]
+    desc_ui = re.compile(r"<DescriptorUI>(D\d+)</DescriptorUI")
+    with open(descriptor_file, "r") as handle:
+        line = handle.readline()
+        while line:
+            if line.startswith("<DescriptorRecord "):
+                while not line.startswith("</DescriptorRecord"):
+                    if line.startswith()
+                    line = handle.readline()
+            line = handle.readline()
 
-desc_records = []
-desc_uis = []
-desc_names = []
-tree_num_lists = []
-min_depths = []
-distinct_tree_posits = []
+def main():
+    # Get command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Pubmed's MeSH descriptor data in XML format", 
+                    required=True, type=str)
+    parser.add_argument("-o", "--output", help="Output file to write data in a tab-delimited format")
+    parser.add_argument("-q", "--quiet", help="Suppress printing of log messages to STDOUT. " \
+                    "Warning: exceptions will not be printed to console", action="store_true")
+    args = parser.parse_args()
 
-# Extract data. tree_num_lists is currently commented out but this might be
-# useful to keep in the future
-for rec in tqdm(records):
-    soup = BeautifulSoup(rec)
-    
-    tree_nums = []
+    # Set up logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler("edge_list_builder.log")
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-    desc_uis.append(soup.descriptorui.string)
-    desc_names.append(soup.descriptorname.find('string').string)
-    if soup.treenumberlist is not None:
-        for tree_num in soup.treenumberlist.find_all('treenumber'):
-            tree_nums.append(tree_num.string)
-        min_depths.append(len(min([t.split(".") for t in tree_nums], key=len)))
-    else:
-        min_depths.append(0)
-    distinct_tree_posits.append(len(tree_nums))
-    tree_num_lists.append(tree_nums)
+    if verbose:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
-with open("../data/mesh_data.tab", "w") as out:
-    for index in range(len(records)):
-        out.write("".join([desc_uis[index], "\t", desc_names[index], "\t"]))
-        out.write("".join([str(min_depths[index]), "\t"]))
-        out.write("".join([str(distinct_tree_posits[index]), "\t"]))
-        out.write(",".join(tree_num_lists[index]))
-        out.write("\n")
+
+if __name__ == "__main__":
+    main()
